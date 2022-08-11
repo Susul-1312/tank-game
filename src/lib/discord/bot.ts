f5f790aa90efa3b33fb162cc2d577742fae56bcb
Susul-1312 <susl@rickroll.link>
2022-08-12 00:31:27 +0200

    Lets push some discord code that wont work, yeehaw

--- src/lib/discord/bot.ts
+++ src/lib/discord/bot.ts
@@ -1,1 +1,103 @@
-// TODO
+import { Gateway } from "@purplet/gateway";
+import { GatewayIntentBits } from "discord-api-types/gateway";
+import { Rest } from "@purplet/rest";
+import Board from "../logic/board";
+import Tank from "../logic/tank";
+import renderPNG from "../render/png";
+import { TankError } from "../shared-types";
+
+const client = new Gateway({
+    token: process.env.DISCORD_TOKEN!,
+    intents: GatewayIntentBits.GuildMessages
+});
+const rest = new Rest({
+    token: process.env.DISCORD_TOKEN!
+});
+
+const board: Board = new Board(10, 10);
+const tanks: Map<string, Tank> = new Map();
+
+client.on("INTERACTION_CREATE", async (interaction) => {
+    console.log(interaction);
+
+    if (!interaction.data) return;
+
+    const data = interaction.data
+
+    // @ts-ignore
+    const interactionName: string = data.name
+
+    if (interactionName == "addtank") {
+        // @ts-ignore
+        const tankname: string = data.options[0].value;
+        console.log(`Adding tank ${tankname}`);
+        const tank = new Tank(tankname);
+        tanks.set(tank.name, tank);
+        board.addTank(tank);
+        respondToInteraction(interaction, `Added tank ${tankname}`);
+    }
+
+    if (interactionName == "removetank") {
+        // @ts-ignore
+        const tankname: string = data.options[0].value;
+        console.log(`Removing tank ${tankname}`);
+        const tank = tanks.get(tankname);
+        if (!tank) {
+            respondToInteraction(interaction, `Tank ${tankname} not found`);
+            return;
+        }
+        board.removeTank(tank);
+        tanks.delete(tank.name);
+        respondToInteraction(interaction, `Removed tank ${tankname}`);
+    }
+
+    if (interactionName == "movetank") {
+        // @ts-ignore
+        const tankname: string = data.options[0].value;
+        // @ts-ignore
+        const direction: string = data.options[1].value;
+        if (direction != "up" && direction != "down" && direction != "left" && direction != "right") {
+            respondToInteraction(interaction, `Invalid direction ${direction}`);
+            return;
+        }
+        const tank = tanks.get(tankname);
+        if (!tank) {
+            respondToInteraction(interaction, `Tank ${tankname} not found`);
+            return;
+        }
+        console.log(`Moving tank ${tankname} ${direction}`);
+        try {
+            board.moveTank(tank, direction);
+            respondToInteraction(interaction, `Moved tank ${tankname} ${direction}`);
+        } catch (e) {
+            if (e instanceof TankError) {
+                respondToInteraction(interaction, e.message);
+            } else
+                throw e;
+        }
+    }
+
+    if (interactionName == "showboard") {
+        console.log(`Rendering board`);
+        const attachment = await renderPNG(board.board);
+        respondToInteraction(interaction, `Rendered board`, attachment);
+    }
+});
+
+function respondToInteraction(interaction: APIInteraction, message: string, attachment?: Buffer): void {
+    console.log(`Responding to interaction ${interaction.id} with message ${message}`);
+    rest.interactionResponse.createInteractionResponse({
+        interactionId: interaction.id,
+        interactionToken: interaction.token,
+        body: {
+            type: 4,
+            data: {
+                content: message
+            }
+        },
+        files: attachment ? [{
+            name: "board.png",
+            data: attachment
+        }] : []
+    })
+}
--- src/lib/save.ts
+++ src/lib/save.ts
@@ -5,15 +5,10 @@ const fileExtension: string = '.tanksav';
 
 export function saveBoard(location: string, board: Board): void {
     const serializedBoard = board.serialize();
-    if (location.startsWith('/'))
-        fs.writeFileSync(`${location}${fileExtension}`, serializedBoard);
-    else
-        fs.writeFileSync(`./${location}${fileExtension}`, serializedBoard);
+    Bun.write(`${location}${fileExtension}`, serializedBoard);
 }
 
-export function loadBoard(location: string): Board {
-    if (location.startsWith('/'))
-        return Board.deserialize(Buffer.from(fs.readFileSync(`${location}${fileExtension}`)));
-    else
-        return Board.deserialize(Buffer.from(fs.readFileSync(`./${location}${fileExtension}`)));
+export async function loadBoard(location: string): Promise<Board> {
+    const serializedBoard = Bun.file(`${location}${fileExtension}`);
+    return Board.deserialize(Buffer.from(await serializedBoard.text()));
 }
