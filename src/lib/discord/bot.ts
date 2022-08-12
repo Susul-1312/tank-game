6560a698d9e5390051e4f68c0b9de5e3642640f5
Susul-1312 <susl@rickroll.link>
2022-08-13 00:59:13 +0200

    pushing some more code that doesnt work *dabs*

--- src/lib/discord/bot.ts
+++ src/lib/discord/bot.ts
@@ -1,5 +1,5 @@
 import { Gateway } from "@purplet/gateway";
-import { GatewayIntentBits } from "discord-api-types/gateway";
+import { GatewayIntentBits, APIInteraction } from "discord-api-types/v10";
 import { Rest } from "@purplet/rest";
 import Board from "../logic/board";
 import Tank from "../logic/tank";
@@ -81,6 +81,82 @@ client.on("INTERACTION_CREATE", async (interaction) => {
         console.log(`Rendering board`);
         const attachment = await renderPNG(board.board);
         respondToInteraction(interaction, `Rendered board`, attachment);
+    }
+
+    if (interactionName == "shoottank") {
+        // @ts-ignore
+        const shootername: string = data.options[0].value;
+        // @ts-ignore
+        const shooteename: string = data.options[1].value;
+        const shooter = tanks.get(shootername);
+        const shootee = tanks.get(shooteename);
+        if (!shooter) {
+            respondToInteraction(interaction, `Tank ${shootername} not found`);
+            return;
+        }
+        if (!shootee) {
+            respondToInteraction(interaction, `Tank ${shooteename} not found`);
+            return;
+        }
+        console.log(`Shooting tank ${shootername} at ${shooteename}`);
+        try {
+            shooter.attack(shootee)
+            respondToInteraction(interaction, `Shot tank ${shootername} at ${shooteename}`);
+        } catch (e) {
+            if (e instanceof TankError) {
+                respondToInteraction(interaction, e.message);
+            } else
+                throw e;
+        }
+    }
+
+    if (interactionName == "transferchips") {
+        // @ts-ignore
+        const sender: string = data.options[0].value;
+        // @ts-ignore
+        const receiver: string = data.options[1].value;
+        // @ts-ignore
+        const amount: number = data.options[2].value;
+        const senderTank = tanks.get(sender);
+        const receiverTank = tanks.get(receiver);
+        if (!senderTank) {
+            respondToInteraction(interaction, `Tank ${sender} not found`);
+            return;
+        }
+        if (!receiverTank) {
+            respondToInteraction(interaction, `Tank ${receiver} not found`);
+            return;
+        }
+        console.log(`Transferring ${amount} chips from ${sender} to ${receiver}`);
+        try {
+            senderTank.transferChips(amount, receiverTank);
+            respondToInteraction(interaction, `Transferred ${amount} chips from ${sender} to ${receiver}`);
+        } catch (e) {
+            if (e instanceof TankError) {
+                respondToInteraction(interaction, e.message);
+            } else
+                throw e;
+        }
+    }
+
+    if (interactionName == "upgrade") {
+        // @ts-ignore
+        const tankname: string = data.options[0].value;
+        const tank = tanks.get(tankname);
+        if (!tank) {
+            respondToInteraction(interaction, `Tank ${tankname} not found`);
+            return;
+        }
+        console.log(`Upgrading tank ${tankname}`);
+        try {
+            tank.upgrade();
+            respondToInteraction(interaction, `Upgraded tank ${tankname}`);
+        } catch (e) {
+            if (e instanceof TankError) {
+                respondToInteraction(interaction, e.message);
+            } else
+                throw e;
+        }
     }
 });
 
